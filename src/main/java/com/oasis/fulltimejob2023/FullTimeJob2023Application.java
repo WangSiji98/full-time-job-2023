24331ea55ad19321818ebeffd363c40f5cd3b901
WangSiji98 <807403713@qq.com>
2023-03-20 23:42:08 -0700

    initial commit

---
+++ src/main/java/com/oasis/fulltimejob2023/FullTimeJob2023Application.java
@@ -0,0 +1,13 @@
+package com.oasis.fulltimejob2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTimeJob2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullTimeJob2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/oasis/fulltimejob2023/FullTimeJob2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.fulltimejob2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTimeJob2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
